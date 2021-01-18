78dbdb0444ce9084774d4e8aa06c600f4f910981
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2021-01-18 22:22:00 +0600

    Создание моделей City и Department

--- OnSale.Common/Enitities/Department.cs
+++ OnSale.Common/Enitities/Department.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
@@ -7,17 +8,17 @@ using System.Threading.Tasks;
 
 namespace OnSale.Common.Enitities
 {
-    public class Country
+    public class Department
     {
         public int Id { get; set; }
 
-        [MaxLength(50,ErrorMessage = "{0} Не может содержать более {1} символов")]
+        [MaxLength(50, ErrorMessage = "{0} Не может содержать более {1} символов")]
         [Required(ErrorMessage = "Обязательное поле")]
         [Display(Name = "Наименование")]
         public string Name { get; set; }
+        public ICollection<City> Cities { get; set; }
 
-        public ICollection<Department> Departments { get; set; }
-
-        public int DepartmentsNumber => Departments == null ? 0 : Departments.Count;
+        [DisplayName("Cities Number")]
+        public int CitiesNumber => Cities == null ? 0 : Cities.Count;
     }
 }
--- OnSale.Web/Data/DataContext.cs
+++ OnSale.Web/Data/DataContext.cs
@@ -10,13 +10,29 @@ namespace OnSale.Web.Data
 
         }
 
+        public DbSet<City> Cities { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
         public DbSet<Country> Countries { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
+            //Создаем модели для базы данных
+
+            //Город
+            modelBuilder.Entity<City>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+
+            //Страна
             modelBuilder.Entity<Country>()
+                .HasIndex(t => t.Name)
+                .IsUnique();
+
+            //Департамент
+            modelBuilder.Entity<Department>()
                 .HasIndex(t => t.Name)
                 .IsUnique();
         }
