4f6c9c7c5c269ac99f9ae2bfc836f890e3c5e777
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2021-01-21 00:00:14 +0600

    Create Edit for Category

--- OnSale.Web/Controllers/CategoriesController.cs
+++ OnSale.Web/Controllers/CategoriesController.cs
@@ -72,5 +72,64 @@ namespace OnSale.Web.Controllers
 
             return View(model);
         }
+
+
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Category category = await _context.Categories.FindAsync(id);
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            CategoryViewModel model = _converterHelper.ToCategoryViewModel(category);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(CategoryViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = model.ImageId;
+
+                if (model.ImageFile != null)
+                {
+                    imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "categories");
+                }
+
+                try
+                {
+                    Category category = _converterHelper.ToCategory(model, imageId, false);
+                    _context.Update(category);
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "There are a record with the same name.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
+            }
+
+            return View(model);
+        }
     }
 }
