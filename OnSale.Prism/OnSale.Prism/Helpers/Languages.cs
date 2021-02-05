5e7c4e26fff385ebf68b9c80e16b6f1f32acc9db
Michael Vassilyev <michaelvassilyevmvc@gmail.com>
2021-02-05 08:26:52 +0600

    Create localization helper

---
+++ OnSale.Prism/OnSale.Prism/Helpers/Languages.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnSale.Common.Helpers;
+using OnSale.Prism.Resources;
+using Xamarin.Forms;
+
+namespace OnSale.Prism.Helpers
+{
+    public static class Languages
+    {
+        static Languages()
+        {
+            CultureInfo ci = DependencyService.Get<ILocalize>().GetCurrentCultureInfo();
+            Resource.Culture = ci;
+            Culture = ci.Name;
+            DependencyService.Get<ILocalize>().SetLocale(ci);
+        }
+
+        public static string Culture { get; set; }
+
+        public static string Accept => Resource.Accept;
+
+        public static string ConnectionError => Resource.ConnectionError;
+
+        public static string Error => Resource.Error;
+    }
+}
